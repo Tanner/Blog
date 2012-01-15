2a53524ac76ee6a28881e758d0d4d50f0a1c3907
Tanner Smith <tanner@tssoftware.net>
2012-01-15 15:07:00 -0500

    Make the j/down and k/up keys move between articles.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+var currentExcerpt = null;
+var excerpts = null;
+
 $(document).ready(function(){
     init();
 })
@@ -8,14 +11,13 @@ function init() {
 		$(this).siblings().removeClass("selected");
 	});
 
-	$(".post-excerpt").click(function() {
-		$(this).addClass("selected");
-		$(this).siblings().removeClass("selected");
+	excerpts = $(".post-excerpt");
 
-		$.get($(this).attr("id"), function(data) {
-			$('#content').html(data);
-		});
+	excerpts.click(function() {
+		selectExcerpt($(this));
 	});
+
+	currentExcerpt = $(".post-exceprt:first");
 
 	// Disable selection - don't show selection cursor
 	$("#sidebar").bind("selectstart", function() {
@@ -26,3 +28,43 @@ function init() {
 		return false;
 	});
 }
+
+function selectExcerpt(excerpt) {
+	if (excerpt == null) {
+		return;
+	}
+
+	currentExcerpt = excerpt;
+
+	excerpt.addClass("selected");
+	excerpt.siblings().removeClass("selected");
+
+	console.log("URL: "+excerpt.attr("id"));
+	$.get(excerpt.attr("id"), function(data) {
+		$('#content').html(data);
+	});
+}
+
+key('j, down', function() {
+	if (currentExcerpt == null) {
+		return;
+	}
+
+	var currentExcerptIndex = excerpts.index(currentExcerpt);
+	if (currentExcerptIndex < excerpts.length - 1) {
+		var nextExcerpt = excerpts.eq(currentExcerptIndex + 1);
+		selectExcerpt(nextExcerpt);
+	}
+});
+
+key('k, up', function() {
+	if (currentExcerpt == null) {
+		return;
+	}
+
+	var currentExcerptIndex = excerpts.index(currentExcerpt);
+	if (currentExcerptIndex > 0) {
+		var prevExcerpt = excerpts.eq(currentExcerptIndex - 1);
+		selectExcerpt(prevExcerpt);
+	}
+});
