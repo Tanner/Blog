6d307b07765cb8bfa4be6694306b96fb6089011b
Tanner Smith <tanner@tssoftware.net>
2013-05-08 14:24:57 -0400

    Update rakefile to new Jekyll commands and terms.

--- rakefile.sample.rb
+++ rakefile.sample.rb
@@ -1,16 +1,17 @@
-desc 'nuke, build and compass'
-task :generate do
+desc 'nuke and build'
+task :build do
   sh 'rm -rf _site'
-  sh 'time jekyll'
+  sh 'time jekyll build'
 end
 
 desc 'build and run test server'
-task :test => [:generate] do
-  sh 'jekyll --auto --server'
+task :serve do
+  sh 'rm -rf _site'
+  sh 'jekyll serve --watch'
 end
 
 desc 'deploy via rsync'
-task :deploy => [:generate] do
+task :deploy do
   puts 'DEPLOYING!'
   sh "rsync -rtzh --progress --delete _site/ user@domain.com:/var/www/site"
   puts 'DONE!'
